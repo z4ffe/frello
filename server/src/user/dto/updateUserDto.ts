2c9f0d2456c140e099ad0cb5de5a0ce61d404b0d
Paul <104695265+z4ffe@users.noreply.github.com>
2023-10-04 16:19:07 +0300

    add: swagger (#1)

--- server/src/user/dto/updateUserDto.ts
+++ server/src/user/dto/updateUserDto.ts
@@ -1,12 +1,16 @@
+import {ApiProperty} from '@nestjs/swagger'
 import {IsNotEmpty} from 'class-validator'
 
 export class UpdateUserDto {
+	@ApiProperty()
 	@IsNotEmpty()
 	username: string
 
+	@ApiProperty()
 	@IsNotEmpty()
 	password: string
 
+	@ApiProperty()
 	@IsNotEmpty()
 	newPassword: string
 }
