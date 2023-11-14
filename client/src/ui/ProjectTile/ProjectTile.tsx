156af67c90a1d3542ee39323ef20fccbc8d81f47
Pavel Kulyasov <pavel.kulyasov@gmail.com>
2023-11-14 19:18:07 +0300

    add: styles for project tiles

--- client/src/ui/ProjectTile/ProjectTile.tsx
+++ client/src/ui/ProjectTile/ProjectTile.tsx
@@ -7,9 +7,21 @@ interface Props {
 }
 
 export const ProjectTile: FC<Props> = ({project}) => {
+	const createdDate = new Date(project.createdAt).toDateString()
+	const randomProgress = Math.round(Math.random() * 100)
+
 	return (
 		<div className={styles.projectTile}>
 			<h2 className={styles.projectTile__title}>{project.name}</h2>
+			<span className={styles.projectTile__date}>{createdDate}</span>
+			<p className={styles.projectTile__desc}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+			<div className={styles.projectTile__progress}>
+				<div className={styles.header}>
+					<h3 className={styles.header__title}>Project progress</h3>
+					<span className={styles.header__percent}>{randomProgress}%</span>
+				</div>
+				<div className={styles.progressBar} style={{width: `${randomProgress}%`}} />
+			</div>
 		</div>
 	)
 }
